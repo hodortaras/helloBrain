c1149d03d4086b8b7abeb00ac8aa2ae665c14ee9
HodorTaras <dvalindvorf@gmail.com>
2019-10-11 16:32:11 +0300

    Добавлен парсинг в Матзадание Brain
    Выбор решаемой задачи передаваемым в командной строке параметром от m1 до m5

--- mathdef.py
+++ mathdef.py
@@ -1,5 +1,9 @@
 import sys
+import argparse
 
+parser = argparse.ArgumentParser(description='Diagram parametrs')
+parser.add_argument('metod', type = str, help = 'Задайте номер решаемого задания "m1....m5"')
+args = parser.parse_args()
 
 def task1 (num, num2):
     s0=num+num2
@@ -30,26 +34,24 @@ def zamena(qq,ww):
         qq=pr2
         ww=pr1
     return qq,ww
-
-a = float(input('Веведите первое действительное число: '))
-b = float(input('Веведите второе действительное число: '))
-l=task1 (a,b)
-print('Сумма чисел {}, разность чисел {}, произведение чисел {}'.format(l[0],l[1],l[2]))
-
-
-rebro = float(input('Введите размер стороны равностороннего треугольника: '))
-print ('Площадь равностороннего треугольника ' + str(square(rebro)))
-
-
-ang=float(input('Радиус сектора 13.7. Введите угол сектора в радианах: '))
-print('Площадь данного сектора: '+str(square1(ang)))
-
-
-a = int(input('Веведите действительное число для возведения в степень: '))
-st = stepen(a)
-print('Число'+str(a)+'в степени 5='+str(st[0]))
-print('Число'+str(a)+'в степени 13='+str(st[1]))
-
-w1 = int(input('Веведите первое действительное число: '))
-w2 = int(input('Веведите второе действительное число: '))
-print('Числа заменены на' +str(zamena(w1,w2)))
+if __name__=='__main__':
+    if args.metod == 'm1':
+        a = float(input('Веведите первое действительное число: '))
+        b = float(input('Веведите второе действительное число: '))
+        l=task1 (a,b)
+        print('Сумма чисел {}, разность чисел {}, произведение чисел {}'.format(l[0],l[1],l[2]))
+    elif args.metod == 'm2':
+        rebro = float(input('Введите размер стороны равностороннего треугольника: '))
+        print ('Площадь равностороннего треугольника ' + str(square(rebro)))
+    elif args.metod == 'm3':
+        ang=float(input('Радиус сектора 13.7. Введите угол сектора в радианах: '))
+        print('Площадь данного сектора: '+str(square1(ang)))
+    elif args.metod == 'm4':
+        a = int(input('Веведите действительное число для возведения в степень: '))
+        st = stepen(a)
+        print('Число'+str(a)+'в степени 5='+str(st[0]))
+        print('Число'+str(a)+'в степени 13='+str(st[1]))
+    elif args.metod == 'm5':
+        w1 = int(input('Веведите первое действительное число: '))
+        w2 = int(input('Веведите второе действительное число: '))
+        print('Числа заменены на' +str(zamena(w1,w2)))
