57e143fb603bcd48a637f31e681657dab07ba7f6
HodorTaras <dvalindvorf@gmail.com>
2020-03-07 23:11:25 +0200

    Add simple model perceptron

--- Natural.py
+++
@@ -1,9 +0,0 @@
-l=int(input('Число гони целое: '))
-x=[1,2]
-for i in range(2,l):
-    for num in range(2,i):
-        if not i%num:
-            break
-        elif num==(i-1):
-            x.append(i)
-print(x)
